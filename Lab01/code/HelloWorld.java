609c1356fd2548944ce9b225be08ddb6107444d8
Cuong-Nong <nongqcuong143@gmail.com>
2025-03-01 08:47:11 +0700

    “Complete assignment 2.2.1

---
+++ Lab01/code/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+	public static void main(String args[]){
+		System.out.println("20235668 - Nong Quoc Cuong");
+		System.out.println("Xin chao \n cac ban!");
+		System.out.println("Hello \t world!");
+	}
+}
