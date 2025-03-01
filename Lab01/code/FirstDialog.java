e5cf5ba12cd8fa624d771d2a1d8782e793786f12
Cuong-Nong <nongqcuong143@gmail.com>
2025-03-01 09:17:46 +0700

    Complete assignment 2.2.2

---
+++ Lab01/code/FirstDialog.java
@@ -0,0 +1,7 @@
+import javax.swing.JOptionPane;
+public class FirstDialog{
+	public static void main(String[] args){
+	JOptionPane.showMessageDialog(null,"20235668 - Nong Quoc Cuong\nHello world! How are you?");
+	System.exit(0);
+	}
+}
